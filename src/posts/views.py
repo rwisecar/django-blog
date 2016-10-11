3bf358f03d1214c322ae2f59066e24ea1dc76f94
Rachael Wisecarver <rachael.wisecarver@gmail.com>
2016-10-11 16:25:14 -0700

    url fixes and html templates

--- src/posts/views.py
+++ src/posts/views.py
@@ -9,7 +9,7 @@ def post_detail(request):
     return HttpResponse("<h1>Please?</h1>")
 
 def post_list(request):
-    return HttpResponse("<h1>ABC</h1>")
+    return render(request, "index.html", {})
 
 def post_update(request):
     return HttpResponse("<h1>Try This:</h1>")
