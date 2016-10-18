852f9ac9fef0c11decf696d24e39e5a139dded1c
Rachael Wisecarver <rachael.wisecarver@gmail.com>
2016-10-18 08:58:47 -0700

    setting up context and queryset properties, displaying post objects using django templates

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,15 +1,33 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Post
 
 
 def post_create(request):
     return HttpResponse("<h1>Hello</h1>")
 
 def post_detail(request):
-    return HttpResponse("<h1>Please?</h1>")
+    context = {
+        "title": "Detail"
+    }
+    return render(request, "index.html", context)
 
 def post_list(request):
-    return render(request, "index.html", {})
+    queryset = Post.objects.all()
+    context = {
+        "object_list": queryset,
+        "title": "list"
+    }
+    # if request.user.is_authenticated():
+    #     context = {
+    #         "title": "My User List"
+    #     }
+    # else:
+    #     context = {
+    #         "title": "List"
+    #     }
+
+    return render(request, "index.html", context)
 
 def post_update(request):
     return HttpResponse("<h1>Try This:</h1>")
