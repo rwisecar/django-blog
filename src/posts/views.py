b51a3d17f3ab6cd30cb15a30ef705f167696732b
Rachael Wisecarver <rachael.wisecarver@gmail.com>
2016-10-18 09:49:12 -0700

    adding django forms to enable creation of new blog posts

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,10 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
+
+from .forms import PostForm
 from .models import Post
 
 
 def post_create(request):
-    return HttpResponse("<h1>Hello</h1>")
+    form = PostForm(request.POST or None)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+
+    context = {
+        "form": form,
+    }
+
+    return render(request, "post_form.html", context)
 
 def post_detail(request, id=None):
     instance = get_object_or_404(Post, id=id)
@@ -18,16 +29,8 @@ def post_list(request):
     queryset = Post.objects.all()
     context = {
         "object_list": queryset,
-        "title": "list"
+        "title": "Blogy McBlogface"
     }
-    # if request.user.is_authenticated():
-    #     context = {
-    #         "title": "My User List"
-    #     }
-    # else:
-    #     context = {
-    #         "title": "List"
-    #     }
 
     return render(request, "index.html", context)
 
