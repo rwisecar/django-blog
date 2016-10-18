f4af920b31252b01da894cc7fa4a64c9cc57992d
Rachael Wisecarver <rachael.wisecarver@gmail.com>
2016-10-18 10:07:42 -0700

    creating update view, adding HttpResponseRedirect to create and update views

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 
 from .forms import PostForm
@@ -10,15 +10,16 @@ def post_create(request):
     if form.is_valid():
         instance = form.save(commit=False)
         instance.save()
+        return HttpResponseRedirect(instance.get_absolute_url())
 
     context = {
         "form": form,
     }
-
     return render(request, "post_form.html", context)
 
 def post_detail(request, id=None):
     instance = get_object_or_404(Post, id=id)
+
     context = {
         "title": instance.title,
         "instance": instance
@@ -31,11 +32,23 @@ def post_list(request):
         "object_list": queryset,
         "title": "Blogy McBlogface"
     }
-
     return render(request, "index.html", context)
 
-def post_update(request):
-    return HttpResponse("<h1>Try This:</h1>")
+def post_update(request, id=None):
+    instance = get_object_or_404(Post, id=id)
+    form = PostForm(request.POST or None, instance=instance)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+        return HttpResponseRedirect(instance.get_absolute_url())
+
+
+    context = {
+        "title": instance.title,
+        "instance": instance,
+        "form": form,
+    }
+    return render(request, "post_form.html", context)
 
 def post_delete(request):
     return HttpResponse("<h1>Exterminate</h1>")
