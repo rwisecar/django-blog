303a0e4e4b1d1744d7d5439035e048bd49040b41
Rachael Wisecarver <rachael.wisecarver@gmail.com>
2016-10-19 13:18:15 -0700

    basic user auth

--- src/posts/views.py
+++ src/posts/views.py
@@ -9,6 +9,8 @@ from .models import Post
 
 
 def post_create(request):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
     form = PostForm(request.POST or None, request.FILES or None)
     if form.is_valid():
         instance = form.save(commit=False)
@@ -50,6 +52,8 @@ def post_list(request):
     return render(request, "post_list.html", context)
 
 def post_update(request, slug=None):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
     instance = get_object_or_404(Post, slug=slug)
     form = PostForm(request.POST or None, request.FILES or None, instance=instance)
     if form.is_valid():
@@ -66,6 +70,8 @@ def post_update(request, slug=None):
     return render(request, "post_form.html", context)
 
 def post_delete(request, slug=None):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
     instance = get_object_or_404(Post, slug=slug)
     instance.delete()
     messages.success(request, "You have successfully deleted your post.")
