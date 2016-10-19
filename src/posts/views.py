cd5a06cf3a593bb38a0645e27e421dd9dd27a4d9
Rachael Wisecarver <rachael.wisecarver@gmail.com>
2016-10-19 09:45:22 -0700

    adding pagination

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,4 +1,5 @@
 from django.contrib import messages
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
 
@@ -28,10 +29,21 @@ def post_detail(request, id=None):
     return render(request, "post_detail.html", context)
 
 def post_list(request):
-    queryset = Post.objects.all()
+    queryset_list = Post.objects.all()
+    paginator = Paginator(queryset_list, 5)
+    page_request_var = "page"
+    page = request.GET.get(page_request_var)
+    try:
+        queryset = paginator.page(page)
+    except PageNotAnInteger:
+        queryset = paginator.page(1)
+    except EmptyPage:
+        queryset = paginator.page(paginator.num_pages)
+
     context = {
         "object_list": queryset,
-        "title": "Rachael's Blog"
+        "title": "Rachael's Blog",
+        "page_request_var": page_request_var
     }
     return render(request, "post_list.html", context)
 
